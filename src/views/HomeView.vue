5b8b6a6579c9f5b1ce2a304c8126a841417da82a
MagnaPax <chunhee.han@outlook.com>
2023-05-26 21:51:33 +0900

    feat: Add basic html structure of home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,20 @@ import Banner from '@/components/Banner.vue';
 </script>
 
 <template>
-  <main>
-    <h1>홈</h1>
+  <section id="banner">
     <Banner />
-  </main>
+  </section>
+  <section class="">
+    <article id="offering">
+      <div>신한은행</div>
+      <div>국민은행</div>
+    </article>
+    <section class="container">
+      <div class="item">1</div>
+      <div class="item">2</div>
+      <div class="item">3</div>
+      <div class="item">4</div>
+      <div class="item">5</div>
+    </section>
+  </section>
 </template>
