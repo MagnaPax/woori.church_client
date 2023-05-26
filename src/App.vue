d1fb35b90cf5b45b0ad3f2cd53a558c11cf51990
MagnaPax <chunhee.han@outlook.com>
2023-05-26 18:56:51 +0900

    feat: Footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
     <!-- RouterView : routes안의 url에 해당하는 컴포넌트들이 표현되는 '태그' -->
     <!-- ToolBar 표시됨 -->
     <RouterView />
+    <Footer />
 </template>
